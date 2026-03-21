accb0a4b4b42aff131050f396ee9aec04da934ff
Vidrimers <vidrimers2@gmail.com>
2026-03-22 02:31:39 +0300

    feat: добавлен ytdownload в список процессов по умолчанию

--- adminuibot.js
+++ adminuibot.js
@@ -129,7 +129,8 @@ const DEFAULT_PM2_PROCESSES = [
   { name: 'vpn-api', pm_id: 2, status: 'online' },
   { name: 'vpn-bot', pm_id: 3, status: 'online' },
   { name: '1xBetLineBoom', pm_id: 4, status: 'online' },
-  { name: 'vidrimers', pm_id: 5, status: 'online' }
+  { name: 'vidrimers', pm_id: 5, status: 'online' },
+  { name: 'ytdownload', pm_id: 6, status: 'online' }
 ];
 
 // Функция для экранирования HTML символов
